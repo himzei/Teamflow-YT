02ad62f54acd2ff575dc0ba6c9f30b32c79812d8
webtest911411@gmail.com <webtest911411@gmail.com>
2025-11-13 08:23:08 +0900

    finished

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -190,14 +190,14 @@ export default function HeroSection() {
               <div className='mask-b-from-55% relative -mr-56 mt-8 overflow-hidden px-2 sm:mr-0 sm:mt-12 md:mt-20'>
                 <div className='inset-shadow-2xs ring-background dark:inset-shadow-white/20 bg-background relative mx-auto max-w-6xl overflow-hidden rounded-2xl border p-4 shadow-lg shadow-zinc-950/15 ring-1'>
                   <Image
-                    className='bg-background aspect-15/8 relative hidden rounded-2xl dark:block object-contain object-top'
+                    className='bg-background aspect-15/8 relative hidden rounded-2xl dark:block object-cover object-top'
                     src={HeroDark}
                     alt='app screen'
                     width='2700'
                     height='1440'
                   />
                   <Image
-                    className='z-2 border-border/25 aspect-15/8 relative rounded-2xl border dark:hidden object-contain object-top'
+                    className='z-2 border-border/25 aspect-15/8 relative rounded-2xl border dark:hidden object-cover object-bottom'
                     src={HeroLight}
                     alt='app screen'
                     width='2700'
